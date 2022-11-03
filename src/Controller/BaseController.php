1f2dd6f6d574d374d0f80beda9d6872b0100c28f
Léane Barbotin <barbotinleane@gmail.com>
2022-11-03 16:21:29 +0100

    Test for sending mails with mailer

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -6,14 +6,29 @@ use App\Repository\FormationRepository;
 use App\Service\FormationResultFormater;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BaseController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(FormationResultFormater $formationResultFormater, FormationRepository $formationRepository): Response
+    public function index(MailerInterface $mailer, FormationResultFormater $formationResultFormater, FormationRepository $formationRepository): Response
     {
         $formations = $formationRepository->findThree();
+
+        $email = (new Email())
+            ->from('leanebn@gmail.com')
+            ->to('leanebn@gmail.com')
+            //->cc('cc@example.com')
+            //->bcc('bcc@example.com')
+            //->replyTo('fabien@example.com')
+            //->priority(Email::PRIORITY_HIGH)
+            ->subject('Time for Symfony Mailer!')
+            ->text('Sending emails is fun again!')
+            ->html('<p>See Twig integration for better HTML integration!</p>');
+
+        $mailer->send($email);
 
         $arrayOfFormations = $formationResultFormater->formationsToArrayWithProgress($formations, $formationRepository);
         return $this->render('base/index.html.twig', [
